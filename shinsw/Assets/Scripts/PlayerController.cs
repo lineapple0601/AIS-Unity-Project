c5ca455f1ac8f0c994798efa0332a1a89f9ec288
申昇祐 <shinsw@ais-info.co.jp>
2019-05-22 22:43:20 +0900

    【申】 Initial Commit

---
+++ shinsw/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // プレイヤーの速度
+    public float moveSpeed;
+    
+    // アニメーター変数
+    private Animator anim;
+    
+    // プレイヤーの移動チェック
+    bool playerMoving;
+
+    // 最後に動いた方向を保存
+    Vector2 lastMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 移動なし
+        playerMoving = false;
+
+        // 横方向の移動
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerMoving = true; // 移動中
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f); // X値の取得
+        }
+            // 縦方向の移動
+        if (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
+        {
+            playerMoving = true; // 移動中
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical")); // Y値の取得
+        }
+            // アニメーションの定義・マッピング
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+}
