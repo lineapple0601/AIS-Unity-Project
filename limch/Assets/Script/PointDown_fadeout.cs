279f59ece0d6ddfc4472ea3c1de88671ccd11141
Lim <cogns523@gmail.com>
2019-06-22 11:24:16 +0900

    UI update

---
+++ limch/Assets/Script/PointDown_fadeout.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointDown_fadeout : MonoBehaviour
+{
+    //pointup時fadeinする機能
+
+    public float speed = -0.01f;
+    float alfa;
+    float red, green, blue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+    }
+
+    void fade()
+    {
+        alfa += speed;
+    }
+}
---
+++ limch/Assets/Script/SplashScene_Play.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene_Play : MonoBehaviour
+{
+    //Sceneをロードする機能
+
+    //public float delayTime = 3;
+    public void OnClick() {
+        SceneManager.LoadScene("Play_Scene");
+    }
+}
