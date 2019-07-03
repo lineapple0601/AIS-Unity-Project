9a692b9229a97d26326f56191b5612f44e2527f7
JohnyanRaS <just.freedom.pg2018@gmail.com>
2019-07-04 04:41:12 +0900

    First Update

---
+++ ais-Hackathon2019/Assets/Scripts/Managers/MainManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeScene();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("get key down d");
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("PauseScene");
+    }
+}
