4d08ef84377dc55b918d5bcff0a5b23cdf5e8ac2
申昇祐 <shinsw@ais-info.co.jp>
2019-07-17 00:56:18 +0900

    push test

---
+++ ais-Hackathon20191/Assets/Scripts/CharController/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float speed = 2.0f;
+
+    public PlayerCtrl_joystick joystick; // PlayerController_Joystickのスクリプト
+    public PauseManager paM;
+
+    public float MoveSpeed; // プレイヤーの移動速度
+
+    private Vector3 _moveVector; // プレイヤーの移動Vector
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = transform; //transform キャシング
+        _moveVector = Vector3.zero; // プレイヤーの移動Vector初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //タッチパッド入力もらい
+        HandleInput();
+
+        /////Start
+        ///キーボードのための操作
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.up * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.up * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            paM.ActivePauseBt();
+        }
+        /////End
+    }
+
+    private void HandleInput()
+    {
+        //タッチパッド入力受信
+        _moveVector = PoolInput();
+    }
+
+    public void FixedUpdate()
+    {
+        //プレイヤー移動
+        Move();
+    }
+
+    private Vector3 PoolInput()
+    {
+        float h = joystick.GetHorizontalValue();
+        float v = joystick.GetVerticalValue();
+        Vector3 moveDir = new Vector3(h, v, 0).normalized;
+
+        return moveDir;
+    }
+
+    public void Move()
+    {
+        _transform.Translate(_moveVector * MoveSpeed * Time.deltaTime);
+    }
+
+}
---
+++ ais-Hackathon20191/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool pauseOn = false;
+    private GameObject pausePanel;
+
+    private void Awake()
+    {
+        
+        pausePanel = GameObject.Find("Joystick").transform.FindChild("PauseUI").gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeScene();
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ActivePauseBt()
+    {
+
+        if (!pauseOn)
+        {
+            //一時的に停止する
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+           
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            pausePanel.SetActive(false);
+            
+        }
+        pauseOn = !pauseOn;
+    }
+
+    public bool getPauseOn()
+    {
+        return this.pauseOn;
+    }
+
+
+}
