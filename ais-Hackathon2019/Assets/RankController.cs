686ef1cc95237902cbfc9d350276ad18e06aaca8
申昇祐 <shinsw@ais-info.co.jp>
2019-08-10 17:43:07 +0900

    テンポラリープッシュ

---
+++ ais-Hackathon2019/Assets/RankController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankController : MonoBehaviour
+{
+    public GameObject Row;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Instantiate(Row);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
