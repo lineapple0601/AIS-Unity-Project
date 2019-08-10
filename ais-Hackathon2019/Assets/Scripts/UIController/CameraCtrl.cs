0c4f2c3ac46f57632b81ddaf513a02d37fd9c7de
Johnyan <just.freedom.pg2018@gmail.com>
2019-08-10 17:35:55 +0900

    JoyStickとキーボードによる船の移動を実装(player_test)

--- ais-Hackathon2019/Assets/Scripts/UIController/CameraCtrl.cs
+++ ais-Hackathon2019/Assets/Scripts/UIController/CameraCtrl.cs
@@ -9,6 +9,7 @@ public class CameraCtrl : MonoBehaviour
     public GameObject Player_2;
     //public GameObject Player_3;
     //public GameObject Player_4;
+    public GameObject Player_Test;
     Transform AT;
 
     public GameObject SeaBackGround;
@@ -80,6 +81,10 @@ public class CameraCtrl : MonoBehaviour
         // {
         //     AT = Player_4.transform;
         //}
+        else if (Player_Test.activeSelf == true)
+        {
+            AT = Player_Test.transform;
+        }
     }
 
     /*
