3e84dc6ccf66311f5289b66b0369bec7860d10a0
JohnyanRaS <just.freedom.pg2018@gmail.com>
2019-08-10 14:20:20 +0900

    移動サンプルを追加(PlayerTest)

---
+++ ais-Hackathon2019/Assets/Scripts/CharController/Joystick.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    // Stick背景イメージ
+    private Image stickBackImg;
+    // Stickイメージ
+    private Image stickImg;
+    // Inputベクトル
+    private Vector3 inputVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //最初に実行するとき、各値をもらう
+        stickBackImg = GetComponent<Image>();
+        stickImg     = transform.GetChild(0).GetComponent<Image>();
+        inputVector = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    /* 
+     * タッチしているとき、発生するOnPointerDown(PointerEventData eventData)メッソードでOnDrag(eventData)実行するようにする。
+     */
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    /* 
+     * bgImgの領域の中でタッチが発生した場合(「RectTransformUtility.ScreenPointToLocalPointInRectangle」これがTureとき)、
+     * タッチされた位置の値をposにあげ、bgImgの直四角形のsizeDeltaでわけ、pos.xは0 ~ -1、pos.yは0 ~ 1の値で作る
+     * joystickImgを基準で左右で動いたとき、pos.xは-1 ~ 1の間の値で、上下で動いた場合pos.y-は1 ~ 1の間の値で変換するため
+     * pos.x * 2 + 1、pos.y * 2 - 1で処理する。
+     * その値をinputVectorに代入し、単位Vectorで作る。最後にjoystickImgをタッチした位置で移動する。
+     */
+    public void OnDrag(PointerEventData eventData)
+    {
+        //この中にタッチされたのが認証できたかを確認
+        //Debug.Log("Joystick >>> OnDrag()");
+
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(stickBackImg.rectTransform,
+                                                                    eventData.position,
+                                                                    eventData.pressEventCamera,
+                                                                    out pos))
+        {
+
+            pos.x = (pos.x / stickBackImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / stickBackImg.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, pos.y * 2 - 1, 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            // 赤いj\Joystickの移動
+            stickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (stickBackImg.rectTransform.sizeDelta.x / 3),
+                                                                     inputVector.y * (stickBackImg.rectTransform.sizeDelta.y / 3));
+        }
+    }
+
+    /* 
+     * タッチを中止したとき、発生するOnPointerUp(PointerEventData eventData)メッソードでinputVectorとjoystickImgの位置を初期化する。
+     */
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector3.zero;
+        stickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    /* 
+     * inputVactor値をPlayerControllerスクリプトに渡すため、使用するGetHorizontalValue
+     * PlayerControllerスクリプトにinputVector.x値をもらうため使用するメッソード
+     */
+    public float GetHorizontalValue()
+    {
+        return inputVector.x;
+    }
+
+    /* 
+     * inputVactor値をPlayerControllerスクリプトに渡すため、使用するGetVerticalValue
+     * PlayerControllerスクリプトにinputVector.y値をもらうため使用するメッソード
+     */
+    public float GetVerticalValue()
+    {
+        return inputVector.y;
+    }
+
+}
---
+++ ais-Hackathon2019/Assets/Scripts/CharController/ShipController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    // 定数
+    public float MAX_ROTATION_ANGLE = 1; // フレーム単位の最大回転角度
+    // 公開変数
+    public int          _hp;            // HP
+    bool _moveFlg;
+    public float        _speed;         // 速度
+    public Vector3      _direction;     // 方向
+    public Joystick     _joystick;      // Joystickコントローラ
+    // 内部変数
+    private float       _rotateAngle;   // 回転角(0.0f~359.0f)
+    private Vector3     _moveVector;    // 船の移動Vector
+    private Transform   _transform;
+    private Rigidbody2D _rd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期配置
+        _speed = 1.5f;
+        transform.position = Vector3.zero;
+        _moveFlg = false;
+        _rotateAngle = -90f; // 初期向き補正：右
+        _direction = Vector3.zero;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, _rotateAngle);
+
+        _rd          = GetComponent<Rigidbody2D>();
+        _transform  = transform;
+        _moveVector = Vector3.zero; // プレイヤーの移動Vector初期化
+    }
+
+    // Update is called once per frame
+    void Update(){}
+
+    // 固定フレームレートによるUpdate
+    public void FixedUpdate()
+    {
+        // 移動情報更新
+        MoveHandler();
+
+        // 移動
+        MoveAction();
+
+        // カメラがプレイヤーを追う
+        // Todo CameraControllerに移す
+        //Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        //if (pos.x < 0f) pos.x = 0f;
+        //if (pos.x > 1f) pos.x = 1f;
+        //if (pos.y < 0f) pos.y = 0f;
+        //if (pos.y > 1f) pos.y = 1f;
+
+        //transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    // 移動をコントロール
+    private void MoveHandler()
+    {
+        _moveFlg = false;
+
+        // JoyStickによる方向設定
+        //GetMoveVectorFromJoystick();
+
+        // キーボードによる方向設定
+        GetMoveVectorFromKeyboard();
+    }
+
+    private void GetMoveVectorFromJoystick()
+    {
+        //float nowAngle = transform.rotation.z;
+        //float targetAngle = nowAngle;
+        //float h = _joystick.GetHorizontalValue();
+        //float v = _joystick.GetVerticalValue();
+
+        //targetAngle = (float)(Math.Atan(v / h) * 180 / Math.PI);
+        //transform.Rotate(0, 0, UpdateRotateZ(nowAngle, targetAngle));
+
+        //Vector3 moveDir = new Vector3(h, v, 0).normalized;
+        //return moveDir;
+    }
+
+    private void GetMoveVectorFromKeyboard()
+    {
+        bool quitflg = false;
+        float nowAngle      = CorrectAngleValue(_rotateAngle);
+        float targetAngle   = nowAngle;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetAngle = 0f;
+            _moveFlg = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            targetAngle = 90f;
+            _moveFlg = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetAngle = 180f;
+            _moveFlg = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetAngle = 270f;
+            _moveFlg = true;
+        }
+        // if (quitflg) { Quit(); }
+        //_rotateAngle = targetAngle;
+        _rotateAngle = CalcRotationAngle(nowAngle, targetAngle);
+    }
+
+    public void MoveAction()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, _rotateAngle);
+        _direction = GetDirectionVectorByAngle(_rotateAngle);
+        // 前進
+        if (_moveFlg)
+        {
+            //_direction = new Vector3(0.5f, 0.7f, 0); //TEST
+            Vector3 newPos = transform.position + _direction * _speed * Time.deltaTime;
+            transform.position = newPos;
+        }
+    }
+
+    // 操作による対象角度への回転を行う場合の回転角を計算（フレーム間最大回転角を考慮）
+    private float CalcRotationAngle(float nowAngle, float targetAngle)
+    {
+        float fixAngle = 0f;
+        float angleDiff = targetAngle - nowAngle;
+
+        if (angleDiff < 0f)
+        {
+            angleDiff = 360f + angleDiff;
+        }
+
+        if (angleDiff < 180f)
+        {
+            if (angleDiff < MAX_ROTATION_ANGLE)
+            {
+                fixAngle = angleDiff;
+            }
+            else
+            {
+                fixAngle = MAX_ROTATION_ANGLE;
+            }
+        }
+        else
+        {
+            angleDiff = 360f - angleDiff;
+            if (angleDiff < MAX_ROTATION_ANGLE)
+            {
+                fixAngle = -angleDiff;
+            }
+            else
+            {
+                fixAngle = -MAX_ROTATION_ANGLE;
+            }
+        }
+        return CorrectAngleValue(nowAngle + fixAngle);
+    }
+
+    private float CorrectAngleValue(float angle)
+    {
+        float fixAngle = angle;
+        while (fixAngle < 0 || fixAngle >= 360)
+        {
+            if (fixAngle < 0)
+            {
+                fixAngle += 360;
+            }
+            else
+            {
+                fixAngle -= 360; 
+            }
+        }
+        return fixAngle;
+    }
+
+    private Vector3 GetDirectionVectorByAngle(float angle)
+    {
+        float posX = - (float)Math.Sin(angle * Math.PI / 180f);
+        float posY = (float)Math.Cos(angle * Math.PI / 180f);
+        //Debug.Log("angle: " + angle + " X: " + X + " Y: " + Y);
+
+        return new Vector3(posX, posY, 0);
+
+        //return Vector3.zero;
+    }
+
+    void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+        UnityEngine.Application.Quit();
+        #endif
+    }
+}
